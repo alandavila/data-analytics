03f857b3986cfce6e101989338f05d89d596660d
Alan Davila <alandavila@gmail.com>
2018-05-10 21:05:16 -0500

    found variables with high correlation

--- d3/explore_data.py
+++ d3/explore_data.py
@@ -61,65 +61,59 @@ us_state_abbrev = {
 import pandas as pd
 import os
 
+#list of dictionaries with question and answer I want to filter the data on 
+health_qa = [{'question':'Ever told you had a stroke?','answer':'Yes'},
+             {'question':'Ever told you had a heart attack (myocardial infarction)?','answer':'Yes'},
+             {'question':'Ever told you had angina or coronary heart disease?','answer':'Yes'},
+             {'question':'Ever told you that you have a form of depression?','answer':'Yes'},
+             {'question':'Ever told you have kidney disease?','answer':'Yes'},
+             {'question':'Ever told you had skin cancer?','answer':'Yes'},
+             {'question':'What is your annual household income?','answer':'Less than $15,000'},
+             {'question':'Do you own or rent your home?','answer':'Rent'}
+            ]
+qlabel = ['stroke', 'heart attack', 'angina', 'depression', 'kidney', 'skin cancer', 'low income', 'rent'] 
+qval_label = [q + ' percent' for q in qlabel]
 
-skin_df = pd.read_csv(os.path.join('resources','BRFSS_2014_Overall_skin_cancer.csv'))
-skin_cancer = skin_df[['Locationabbr','Data_value']][skin_df['Response'] == 'Yes']
-skin_cancer.reset_index(inplace=True)
-#drop US, Puerto Rico, Guam and UW
-skin_cancer = skin_cancer.drop(skin_cancer.index[[51,52,53,54]])
-skin_cancer['State'] = skin_cancer['Locationabbr']
-skin_cancer['cancer_percent'] = skin_cancer['Data_value']
-skin_cancer = skin_cancer[['State','cancer_percent']]
+#read in csv file --> pandas dataframe
+health_df_in= pd.read_csv(os.path.join('resources','BRFSS_2014_Overall_Own_or_Rent.csv'))
+#filter columns necessary to query data
+health_df_in = health_df_in[['Locationabbr','Question', 'Response', 'Data_value']]
+#filter the desired rows
+df_list = []
+for qa, label, qval in zip(health_qa,qlabel, qval_label):
+    question, answer = qa['question'], qa['answer']
+    temp_df = health_df_in[health_df_in['Question'] ==  question]
+    temp_df = temp_df[temp_df['Response'] == answer]
+    temp_df[label] = temp_df['Question']
+    temp_df[qval] = temp_df['Data_value']
+    temp_df['State'] = temp_df['Locationabbr']
+    df_list.append(temp_df)
 
+merged_df = df_list[0]
+merged_df = merged_df[['State']]
+for df in df_list:
+    merged_df = pd.merge(merged_df, df, how='inner', on ='State')
 
-rent_df = pd.read_csv(os.path.join('resources','BRFSS_2014_Overall_Own_or_Rent.csv'))
-rent_df = rent_df[rent_df['Question'] == 'Do you own or rent your home?']
-rent_df = rent_df[rent_df['Response'] == 'Rent']
-rent_df.index = range(0,55)
-rent_df = rent_df[['Locationabbr','Data_value']]
-rent_df = rent_df.drop(rent_df.index[[51,52,53,54]])
-rent_df['State'] = rent_df['Locationabbr']
-rent_df['rent_percent'] = rent_df['Data_value']
-rent_df = rent_df[['State','rent_percent']]
+cols = qval_label + ['State']
+merged_df = merged_df[cols]
+merged_df = merged_df.iloc[0:51] #remove us, puerto rico, etc
 
-#dictionary of health factors to investigate in form of desired quesiton:answer
-health_qa = [('Ever told you had a stroke?','Yes'),
-             ('Ever told you had a heart attack (myocardial infarction)?','Yes'),
-             ('Ever told you had angina or coronary heart disease?','Yes'),
-             ('Ever told you that you have a form of depression?','Yes'),
-             ('Ever told you have kidney disease?','Yes'),
-             ('Ever told you had skin cancer?','Yes'),
-             ('What is your annual household income?','Less than $15,000'),
-             ('Do you own or rent your home?','Rent')
-             ]
-health_df = pd.read_csv(os.path.join('resources','BRFSS_2014_Overall_Own_or_Rent.csv'))
-#filter out df data to include desired questions/answers only
-health_df = health_df[health_df.Question == health_qa]
-#for question, answer in health_qa.items():
-#    health_df
-
-
-
-#join cancer rate and rent rate in same dataframe
-merged_df = pd.merge(skin_cancer, rent_df, how='inner', on='State')
-
-
-foreign_df = pd.read_csv(os.path.join('resources','ACS_14_1YR_S0502','ACS_14_1YR_S0502_with_ann.csv'))
-#State, percent of rented occupied housing units, percent of latinos
-foreign_df = foreign_df[['GEO.display-label','HC01_EST_VC169','HC01_EST_VC41']]
+foreign_df = pd.read_csv(os.path.join('resources','ACS_14_1YR_S0102','ACS_14_1YR_S0102_with_ann.csv'))
+#State, percentage of people over 60 with disabilities
+foreign_df = foreign_df[['GEO.display-label','HC02_EST_VC63']]
 foreign_df['State'] = foreign_df['GEO.display-label']
-foreign_df['foreign_rent_percent'] = foreign_df['HC01_EST_VC169']
-foreign_df['latino_percent'] = foreign_df['HC01_EST_VC41']
-foreign_df = foreign_df[['State','foreign_rent_percent','latino_percent']][1:]
+foreign_df['families_poverty'] = foreign_df['HC02_EST_VC63']
+#foreign_df = foreign_df[['State','foreign_rent_percent','latino_percent']][1:]
 foreign_df = foreign_df.replace({'State':us_state_abbrev})
-
+#
 merged_df = pd.merge(merged_df, foreign_df, how='inner', on='State')
-merged_df['foreign_rent_percent'] = merged_df['foreign_rent_percent'].apply(lambda x: float(x)) 
-merged_df['latino_percent'] = merged_df['latino_percent'].apply(lambda x: float(x)) 
-
-merged_df.plot.scatter(x='rent_percent', y='foreign_rent_percent')
-corr = merged_df[['rent_percent','foreign_rent_percent']].corr(method='pearson')
-print(corr)
+merged_df['families_poverty'] = merged_df['families_poverty'].apply(lambda x: float(x)) 
+#
+for issue in qval_label:
+    merged_df.plot.scatter(x=issue, y='families_poverty')
+    corr = merged_df[[issue,'families_poverty']].corr(method='pearson')
+    print(corr)
+    x = input('continue?')
 
 #merged_df.plot.scatter(x='cancer_percent', y='latino_percent')
 #corr = merged_df[['cancer_percent','latino_percent']].corr(method='pearson')
